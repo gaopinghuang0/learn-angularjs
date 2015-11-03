03069ca66d9dc02ab46d225b64303f4752f2aac8
gaopinghuang0 <gaopinghuang@gmail.com>
2015-11-02 22:19:23 -0500

    finish formCtrl and validateCtrl

--- w3cschool/js/controllers.js
+++ w3cschool/js/controllers.js
@@ -1,9 +1,12 @@
-var app = angular.module('myApp', []);
 app.controller('personCtrl', function($scope) {
 	$scope.firstName = "John";
 	$scope.lastName = "Doe";
+	$scope.myVar = false;
 	$scope.fullName = function() {
 		return $scope.firstName + " " + $scope.lastName;
+	}
+	$scope.toggle = function() {
+		$scope.myVar = !$scope.myVar;
 	}
 });
 
@@ -26,3 +29,25 @@ app.controller("customersCtrl", function($scope, $http) {
 	$http.get("http://www.w3schools.com/angular/customers.php")
 	.success(function(response) {$scope.names = response.records;});
 });
+
+
+app.controller("myCtrl", function($scope) {
+	$scope.count = 0;
+});
+
+
+app.controller("formCtrl", function($scope) {
+	$scope.master = {firstName: 'John', lastName: "Doe"};
+	$scope.reset = function() {
+		$scope.user = angular.copy($scope.master);
+	};
+	$scope.reset();
+});
+
+
+app.controller("validateCtrl", function($scope) {
+	$scope.user = 'John Doe';
+	$scope.email = 'John.doe@gmail.com';
+});
+
+
